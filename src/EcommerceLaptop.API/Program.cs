4b5701d85a6ad9f2b6b5a16f996c8d77b866df86
QuocQQuang <149650064+QuocQQuang@users.noreply.github.com>
2025-12-15 19:44:48 +0700

    a

--- src/EcommerceLaptop.API/Program.cs
+++ src/EcommerceLaptop.API/Program.cs
@@ -62,6 +62,8 @@ builder.Services.AddControllers()
         options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter(System.Text.Json.JsonNamingPolicy.CamelCase));
     });
 
+builder.Services.AddSignalR();
+
 // CORS
 builder.Services.AddCors(options =>
 {
